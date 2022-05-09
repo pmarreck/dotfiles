bcaf11248c4892c280a7bf2eb384c18165b81575
Peter Marreck <lumbergh@gmail.com>
2022-05-09 15:13:04 -0400

    overrode the color scheme's scrollbar thumb color

--- .wezterm.lua
+++ .wezterm.lua
@@ -1,7 +1,15 @@
 local wezterm = require 'wezterm';
+-- see color schemes with examples at https://wezfurlong.org/wezterm/colorschemes/index.html
+local scheme = wezterm.get_builtin_color_schemes()["Cobalt2"]
+-- overrides
+scheme.scrollbar_thumb = "#aaa"
+
 return {
   font = wezterm.font("Berkeley Mono"),
-  color_scheme = "Cobalt2",
+  color_schemes = {
+    ["Cobalt Peter"] = scheme,
+  },
+  color_scheme = "Cobalt Peter",
   font_size = 15.0,
   window_background_opacity = 0.85,
   hide_tab_bar_if_only_one_tab = true,
