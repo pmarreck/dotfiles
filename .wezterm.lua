3bfdea17351745de51975dc3be3ea265d2b92e78
Peter Marreck <lumbergh@gmail.com>
2022-06-04 13:57:12 -0400

    tweak wezterm look and feel settings

--- .wezterm.lua
+++ .wezterm.lua
@@ -12,7 +12,30 @@ return {
   color_scheme = "Cobalt Peter",
   font_size = 15.0,
   window_background_opacity = 0.85,
+  use_fancy_tab_bar = false,
   hide_tab_bar_if_only_one_tab = true,
   enable_scroll_bar = true,
   scrollback_lines = 1000000,
+  -- https://wezfurlong.org/wezterm/config/lua/config/window_frame.html
+  window_frame = {
+    inactive_titlebar_bg = "#353535",
+    active_titlebar_bg = "#2b2042",
+    inactive_titlebar_fg = "#cccccc",
+    active_titlebar_fg = "#ffffff",
+    inactive_titlebar_border_bottom = "#2b2042",
+    active_titlebar_border_bottom = "#2b2042",
+    button_fg = "#cccccc",
+    button_bg = "#2b2042",
+    button_hover_fg = "#ffffff",
+    button_hover_bg = "#3b3052",
+  },
+  -- https://wezfurlong.org/wezterm/config/lua/config/window_padding.html
+  window_padding = {
+    left = "1cell",
+    right = "1.5cell",
+    top = "0.5cell",
+    bottom = "0.5cell",
+  },
+  -- https://wezfurlong.org/wezterm/config/lua/config/window_close_confirmation.html
+  window_close_confirmation = "NeverPrompt",
 }
