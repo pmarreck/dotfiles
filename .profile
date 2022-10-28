6c4d2a3de4bcdbd3ab18952171250903579cd9a9
Peter Marreck <lumbergh@gmail.com>
2022-10-28 13:09:48 -0400

    polish define function and separate it out; also disable where function as it's now superseded

--- .profile
+++ .profile
@@ -171,69 +171,17 @@ source $HOME/bin/randompass.sh
 export GPG_TTY=`tty`
 
 # which hack, so it also shows defined aliases and functions that match
-where() {
-  type_out=`type "$@"`;
-  if [ ! -z "$type_out" ]; then
-    echo "$type_out";
-  else
-    /usr/bin/env which $@;
-  fi
-}
+# where() {
+#   type_out=`type "$@"`;
+#   if [ ! -z "$type_out" ]; then
+#     echo "$type_out";
+#   else
+#     /usr/bin/env which $@;
+#   fi
+# }
+# Note: Superseded by "define" function below
 
-# spit out the definition of a function or alias
-# usage: define <function or alias name> [...function|alias] ...
-# Useful to do things like exporting definitions to sudo context
-# or seeing if one definition is masking another
-define() {
-  local word="$1"
-  shift
-  if [ -z "$word" ] && [ -z "$1" ]; then
-    echo "Usage: define <function or alias name> [...function|alias] ..."
-    return 1
-  fi
-  if $(env | grep -q "^$word="); then
-    note "$word is an environment variable"
-    env | grep --color=never "^$word="
-  elif [ -z "$(type -a -t $word)" ]; then
-    warn "$word is undefined"
-  else
-    # if there are multiple types to search for, loop through them
-    for type in $(type -a -t $word | uniq); do
-      case $type in
-        builtin)
-          note "$word is a builtin"
-          ;;
-        function)
-          note "$word is a function"
-          # replace runs of 2 spaces with 1 space
-          # and format the function definition the way I like it
-          declare -f $word |\
-            sed -z 's/\n{/ {/' |\
-            sed 's/  / /g' |\
-            sed -E 's/^([_[:alpha:]][_[:alnum:]]*)\s\(\)/\1()/'
-          ;;
-        alias)
-          note "$word is an alias"
-          alias $word
-          ;;
-        file)
-          # if it's a file, just print the path
-          note "$word is at least one executable file in PATH"
-          type -a -p $word | uniq
-          ;;
-        *)
-          note "$word is not a builtin, function, alias, or file; it is a $type"
-          # return 1
-          ;;
-      esac
-    done
-  fi
-  # if there are any words left to look up, recurse with them
-  if [ ! -z "$1" ]; then
-    define $@
-  fi
-}
-
+source $HOME/bin/define.sh
 
 # elixir and js lines of code count
 # removes blank lines and commented-out lines
---
+++ bin/define.sh
@@ -0,0 +1,67 @@
+#!/usr/bin/env bash
+
+# alerting in yellow to stderr
+$(declare -F note >/dev/null) || note() {
+  >&2 printf "\e[0;33m%s\e[0;39m\n" "$@"
+}
+# warning in red to stderr
+$(declare -F warn >/dev/null) || warn() {
+  >&2 printf "\e[0;31m%s\e[0;39m\n" "$@"
+}
+
+# "define": spit out the definition of any name
+# usage: define <function or alias or variable or builtin or executable-in-PATH name> [...function|alias] ...
+# It will dig out all definitions, helping you find things like overridden bins.
+# Also useful to do things like exporting specific definitions to sudo contexts etc.
+# or seeing if one definition is masking another.
+define() {
+  local word="$1"
+  shift
+  if [ -z "$word" ] && [ -z "$1" ]; then
+    echo "Usage: define <function or alias or variable or builtin or executable-in-PATH name> [...function|alias] ..."
+    echo "This function is defined in $BASH_SOURCE"
+    return 0
+  fi
+  if $(env | grep -q "^$word="); then
+    note "$word is an environment variable"
+    env | grep --color=never "^$word="
+  elif [ -z "$(type -a -t $word)" ]; then
+    warn "$word is undefined"
+  else
+    # if there are multiple types to search for, loop through them
+    for type in $(type -a -t $word | uniq); do
+      case $type in
+        builtin)
+          note "$word is a builtin"
+          ;;
+        function)
+          note "$word is a function"
+          # replace runs of 2 spaces with 1 space
+          # and format the function definition the way I like it
+          declare -f $word |\
+            sed -z 's/\n{/ {/' |\
+            sed 's/  / /g' |\
+            sed -E 's/^([_[:alpha:]][_[:alnum:]]*)\s\(\)/\1()/'
+          ;;
+        alias)
+          note "$word is an alias"
+          alias $word
+          ;;
+        file)
+          # if it's a file, just print the path
+          note "$word is at least one executable file in PATH"
+          type -a -p $word | uniq
+          ;;
+        *)
+          # things should not get here; if they do, add a case for them above
+          note "$word is not a variable, builtin, function, alias, or file; it is a $type"
+          # return 1
+          ;;
+      esac
+    done
+  fi
+  # if there are any words left to look up, recurse with them
+  if [ ! -z "$1" ]; then
+    define $@
+  fi
+}
