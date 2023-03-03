241de74aab5a587ccbd8cfeb4c5cf05d9c53457c
Peter Marreck <lumbergh@gmail.com>
2023-03-03 15:13:05 -0500

    Added a timeout to the curl inside the ask function and cleaned up some indentation.

--- bin/functions/ask.sh
+++ bin/functions/ask.sh
@@ -20,11 +20,12 @@ ask() {
 # printf "escaped args: %s\n" "$args" >&2
   # note that gpt-3.5-turbo-0301 is the very latest model as of 2021-03-01 but will only be supported for a few weeks
   read -r -d '' request <<EOF
-  curl https://api.openai.com/v1/chat/completions\
- -H "Authorization: Bearer $OPENAI_API_KEY"\
- -H "Content-Type: application/json"\
- --silent\
- -d '{"model": "gpt-3.5-turbo-0301", "messages": [{"role": "user", "content": $args}]}'
+  curl https://api.openai.com/v1/chat/completions \
+  -H "Authorization: Bearer $OPENAI_API_KEY" \
+  -H "Content-Type: application/json" \
+  --silent \
+  --max-time 10 \
+  -d '{"model": "gpt-3.5-turbo-0301", "messages": [{"role": "user", "content": $args}]}'
 EOF
 # printf "request: %s\n" "$request" >&2
   response=$(eval $request)
