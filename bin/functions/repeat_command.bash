ac986f1ee7a9381fe6a17f3219be6e243d5f80ec
Peter Marreck <lumbergh@gmail.com>
2023-05-10 11:12:03 -0400

    Add error code summing to repeat function and include test case.

--- bin/functions/repeat_command.bash
+++ bin/functions/repeat_command.bash
@@ -1,13 +1,18 @@
 # usage: repeat <number of times> <command>
 repeat() {
   local count=$1
+  local retcodes=0
   shift
   cmd=($@)
   for ((i = 0; i < count; i++)); do
     eval "${cmd[@]}"
+    (( retcodes+=$? ))
   done
+  return $retcodes
 }
 
 source_relative_once bin/functions/assert.bash
 
 assert "$(repeat 3 "echo -n \"hi \"")" == 'hi hi hi '
+# ensure return code is summed up from failed commands
+assert "$(repeat 3 false; echo $?)" == "3"
