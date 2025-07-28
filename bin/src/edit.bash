f1bdfe7452259dc51a84ab8c730e0dd37e1fc5a3
Peter Marreck <lumbergh@gmail.com>
2025-07-28 14:25:08 -0400

    Refactor executables function to improve performance and add script filtering
    Add command line argument parsing and use fd for faster searching if available
    
    Update edit function to handle script files and executables more intelligently
    
    Remove old executables.bash script and add new test suite for executables

--- bin/src/edit.bash
+++ bin/src/edit.bash
@@ -2,10 +2,23 @@
 
 edit() {
 	[ -n "${EDIT}" ] && unset EDIT && edit_function "${FUNCNAME[0]}" "$BASH_SOURCE" && return
-	if contains "$(functions)" $1; then
+	# if the file is in the current directory, edit it
+	if [ -f "$1" ]; then
+		choose_editor "$1"
+	# if the file is a function, edit it by running it with EDIT=1 (which in my bash functions will fire up an editor at that line)
+	elif contains "$(functions)" $1; then
 		EDIT=1 $1
+	# if the file is an executable, edit it
+	elif contains "$(executables --scripts)" $1; then
+		local full_path=$(which "$1")
+		# search for presence of "unset EDIT" in file to assume it uses my pattern of firing up an editor for that file or function if EDIT is set and it is run
+		if grep -q "unset EDIT" "$full_path"; then
+			EDIT=1 "$1"
+		else
+			choose_editor "$full_path"
+		fi
 	else
-		choose_editor "$@"
+		choose_editor "$1"
 	fi
 }
 
