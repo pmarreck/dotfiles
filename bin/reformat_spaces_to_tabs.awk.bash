2ba503fad03a935cd6c61fae6cc265078e980112
Peter Marreck <lumbergh@gmail.com>
2026-08-27 22:56:45 -0400

    Harden reformat_spaces_to_tabs inference and streaming

--- bin/reformat_spaces_to_tabs.awk.bash
+++ bin/reformat_spaces_to_tabs.awk.bash
@@ -37,8 +37,7 @@ readonly AWK_BIN
 
 resolve_backup_path() {
 	local file="$1"
-	local base
-	base=$(basename "$file")
+	local base="${file##*/}"
 	local candidate="$BACKUP_ROOT/${base}.maybe-no-tabs"
 	local suffix=1
 	while [ -e "$candidate" ]; do
@@ -49,75 +48,118 @@ resolve_backup_path() {
 }
 determine_tab_size_in_spaces() {
 	local file="$1"
+	# shellcheck disable=SC2016 # This is an Awk program, not shell interpolation.
 	"$AWK_BIN" '
+	function ranks_before(left, right) {
+		if (right == 0) {
+			return 1;
+		}
+		if (space_indents[left] != space_indents[right]) {
+			return space_indents[left] > space_indents[right];
+		}
+		return (left + 0) < (right + 0);
+	}
+	function rank_width(width, position, displaced) {
+		for (position = 1; position <= 3; position++) {
+			if (ranks_before(width, ranked_widths[position])) {
+				displaced = ranked_widths[position];
+				ranked_widths[position] = width;
+				width = displaced;
+				if (width == 0) {
+					return;
+				}
+			}
+		}
+	}
 	{
-		if (length($0) > 0 && ($0 ~ /^ / || $0 ~ /^\t/)) {
-			match($0, /^ */);
-			if (RLENGTH > 0) {
-				space_indents[RLENGTH]++;
-			}
-			match($0, /^\t*/);
-			if (RLENGTH > 0) {
-				tab_indents[RLENGTH]++;
-			}
+		if ($0 ~ /^[ \t]*\r?$/) {
+			next;
+		}
+		match($0, /^\t*/);
+		tab_count = RLENGTH;
+		if (tab_count > 0) {
+			tabbed_lines++;
+		}
+		remaining = substr($0, tab_count + 1);
+		match(remaining, /^ */);
+		space_count = RLENGTH;
+		if (space_count > 0) {
+			spaced_lines++;
+			space_indents[space_count]++;
 		}
 	}
 	END {
 		for (indent in space_indents) {
-			if (indent > 0 && space_indents[indent] > max_space_count) {
-				third_space = second_space;
-				second_space = max_space;
-				max_space = indent;
-				max_space_count = space_indents[indent];
-			}
+			rank_width(indent);
 		}
-		for (indent in tab_indents) {
-			if (indent > 0 && tab_indents[indent] > max_tab_count) {
-				third_tab = second_tab;
-				second_tab = max_tab;
-				max_tab = indent;
-				max_tab_count = tab_indents[indent];
-			}
-		}
-		if (max_tab_count > max_space_count) {
+		if (tabbed_lines > spaced_lines) {
 			print 0;
 			exit;
 		}
-		if (max_space == 0) {
+		if (ranked_widths[1] == 0) {
 			print 0;
 			exit;
 		}
 		div_count = split("8 7 6 5 4 3 2", divs, " ");
 		for (i = 1; i <= div_count; i++) {
 			d = divs[i];
-			if (max_space % d == 0 &&
-				(second_space == 0 || second_space % d == 0) &&
-				(third_space == 0 || third_space % d == 0)) {
+			divides_all = 1;
+			for (position = 1; position <= 3; position++) {
+				width = ranked_widths[position];
+				if (width != 0 && width % d != 0) {
+					divides_all = 0;
+					break;
+				}
+			}
+			if (divides_all) {
 				print d;
 				exit;
 			}
 		}
-		print 1;
+		print 0;
 	}
 	' "$file"
 }
+
+is_text_file() {
+	local file="$1"
+	if [ ! -s "$file" ]; then
+		return 0
+	fi
+
+	local mime_type
+	mime_type=$(file -b --mime-type -- "$file") || return 1
+	case "$mime_type" in
+		text/*|application/json|application/xml|application/*+json|application/*+xml)
+			return 0
+			;;
+		*)
+			return 1
+			;;
+	esac
+}
+
 process_file() {
 	local file="$1"
 	local in_place="$2"
+	local filesystem_path="$file"
+	if [[ "$filesystem_path" == -* ]]; then
+		filesystem_path="./$filesystem_path"
+	fi
 
-	if ! file -b --mime-type "$file" | grep -q '^text/'; then
+	if ! is_text_file "$filesystem_path"; then
 		echo "Warning: Not a text file: $file" >&2
 		return 1
 	fi
 
 	local indent
-	indent=$(determine_tab_size_in_spaces "$file")
+	indent=$(determine_tab_size_in_spaces "$filesystem_path")
 
 	if [ "${indent:-0}" -le 0 ]; then
 		if [ "$in_place" = true ]; then
 			echo "No changes needed for: $file" >&2
 		else
-			cat "$file"
+			cat "$filesystem_path"
 		fi
 		return 0
 	fi
@@ -130,41 +172,66 @@ process_file() {
 		local backup_file
 		backup_file=$(resolve_backup_path "$file")
 		mkdir -p "$(dirname "$backup_file")"
-		cp -p "$file" "$backup_file"
+		cp -p "$filesystem_path" "$backup_file"
 		printf 'Backup stored for %s at %s\n' "$file" "$backup_file" >&2
 
-		local stat_bin stat_fmt
+		local stat_bin
+		local -a stat_args
 		if command -v gstat >/dev/null 2>&1; then
 			stat_bin=$(command -v gstat)
-			stat_fmt='-c %a'
+			stat_args=(-c '%a')
 		else
 			stat_bin=$(command -v stat)
-			stat_fmt='-f %A'
+			if "$stat_bin" --version 2>/dev/null | grep -q 'GNU coreutils'; then
+				stat_args=(-c '%a')
+			else
+				stat_args=(-f '%Lp')
+			fi
 		fi
 		local perms
-		perms=$($stat_bin $stat_fmt "$file")
+		perms=$("$stat_bin" "${stat_args[@]}" "$filesystem_path")
 
-		"$AWK_BIN" -v indent="$indent_spaces" '
+		local replacement_template="${filesystem_path}.tmp.XXXXXX"
+		if [[ "$replacement_template" == -* ]]; then
+			replacement_template="./$replacement_template"
+		fi
+		local replacement_path
+		replacement_path=$(mktemp "$replacement_template")
+		if ! cp -p "$filesystem_path" "$replacement_path"; then
+			unlink "$replacement_path"
+			return 1
+		fi
+
+		# shellcheck disable=SC2016 # This is an Awk program.
+		if ! "$AWK_BIN" -v indent="$indent_spaces" '
 		{
-			while (match($0, "^\t*" indent)) {
-				$0 = gensub("^(\t*)" indent, "\\1\t", "g");
+			if ($0 !~ /^[ \t]*\r?$/) {
+				while (match($0, "^\t*" indent)) {
+					$0 = gensub("^(\t*)" indent, "\\1\t", "g");
+				}
 			}
 			print;
 		}
-		' "$file" >"$file.new"
+		' "$filesystem_path" >"$replacement_path"; then
+			unlink "$replacement_path"
+			return 1
+		fi
 
-		chmod "$perms" "$file.new"
-		mv "$file.new" "$file"
+		chmod "$perms" "$replacement_path"
+		mv "$replacement_path" "$filesystem_path"
 		echo "Reformatted: $file (indent size: $indent)"
 	else
+		# shellcheck disable=SC2016 # This is an Awk program.
 		"$AWK_BIN" -v indent="$indent_spaces" '
 		{
-			while (match($0, "^\t*" indent)) {
-				$0 = gensub("^(\t*)" indent, "\\1\t", "g");
+			if ($0 !~ /^[ \t]*\r?$/) {
+				while (match($0, "^\t*" indent)) {
+					$0 = gensub("^(\t*)" indent, "\\1\t", "g");
+				}
 			}
 			print;
 		}
-		' "$file"
+		' "$filesystem_path"
 	fi
 }
 run_tests() {
